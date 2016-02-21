10a495b29f1440e80d6ec349cb1ce4d07f96b202
Josermando Peralta <jo.peralta15@gmail.com>
2016-02-21 15:14:05 -0400

    Populating ListView with API Data

---
+++ app/src/main/java/com/josermando/apps/carfinder/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package com.josermando.apps.carfinder;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Josermando Peralta on 2/19/2016.
+ */
+public class MainActivityFragment extends Fragment{
+    public MainActivityFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
